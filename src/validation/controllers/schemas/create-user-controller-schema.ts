2981fb0fa031cf43590591b07a31105906fb8903
iguinn13 <igoor182@hotmail.com>
2024-10-03 13:45:16 -0300

    refactor: changing error message

--- src/validation/controllers/schemas/create-user-controller-schema.ts
+++ src/validation/controllers/schemas/create-user-controller-schema.ts
@@ -1,7 +1,7 @@
 import zod from 'zod';
 
 export const createUserControllerSchema = zod.object({
-    name: zod.string({ message: 'Name is necessary' }),
+    name: zod.string({ message: 'Name must be provided' }),
     email: zod.string().email({ message: 'Email must be provided' }),
     password: zod.string({ message: 'Password must be provided' }),
 });
